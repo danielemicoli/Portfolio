e055c9aa1037a777cee518c31ced2e1592357bc0
danielemicoli <danielemicoli@gmail.com>
2021-08-29 11:42:44 +0200

    my2commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,72 @@
-// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-//     target: '#navbar-example'
-//   })
+window.onload = function(){
+    const effect = document.querySelector('#effect');
+    const effectAbout = document.querySelector('#effect-about');
+    const effectServices = document.querySelector('#effect-services');
+    const effectSkills = document.querySelector('#skills');
+
+
+    window.addEventListener('scroll', scrollEffect);
+
+function scrollEffect(){
+    if(window.scrollY>=500){
+        effect.style.opacity = '0';
+        effect.style.transform = 'translateY(-7em)';
+        effect.style.transition = '0s ease-in-out';
+    }
+    else{
+        effect.style.opacity = '1';
+        effect.style.transform = 'translateY(0)';
+        effect.style.transition = '1.5s ease-in-out';
+
+
+
+
+    }
+}
+    window.addEventListener('scroll', scrollEffectAbout);
+
+    function scrollEffectAbout(){
+        if(window.scrollY>=600){
+            effectAbout.style.opacity = '1';
+            effectAbout.style.transform = 'translateX(0)';
+            effectAbout.style.transition = '1.5s ease-in-out';
+        }
+        else{
+            effectAbout.style.opacity = '0';
+            effectAbout.style.transform = 'translateX(1em)';
+    
+    
+    
+        }
+    }
+    window.addEventListener('scroll', scrollEffectServices);
+
+    function scrollEffectServices(){
+        if(window.scrollY>=1300){
+            effectServices.style.opacity = '1';
+            effectServices.style.transform = 'translateY(0)';
+            effectServices.style.transition = '1.3s ease-in-out';
+        }
+        else{
+            effectServices.style.opacity = '0';
+            effectServices.style.transform = 'translateY(9em)';
+    
+    
+    
+        }
+    }
+    function scrollEffect(){
+        if(window.scrollY>=2000){
+            effectSkills.style.opacity = '1';
+            effectSkills.style.transform = 'translateY(0)';
+            effectSkills.style.transition = '1.5s ease-in-out';
+        }
+        else{
+            effectSkills.style.opacity = '0';
+            effectSkills.style.transform = 'translateY(-7em)';
+    
+    
+    
+        }
+    }
+}
